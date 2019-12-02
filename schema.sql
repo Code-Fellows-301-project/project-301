6e2f70b66637308fa161945767165411a26755d5
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-12-02 22:36:59 +0200

    end-to-end flow

--- schema.sql
+++ schema.sql
@@ -1,14 +1,11 @@
 DROP TABLE IF EXISTS recipe;
 CREATE TABLE IF NOT EXISTS recipe (
- id SERIAL PRIMARY KEY,
- mealsName VARCHAR(255),
+  id SERIAL PRIMARY KEY,
+  mealsname VARCHAR(255),
   category VARCHAR(255),
   area VARCHAR(255),
   instruction TEXT,
-  imageUrl VARCHAR(255)
-  youtube VARCHAR(255)
-  
+  imageurl VARCHAR(255),
+  youtube VARCHAR(255)  
 );
-INSERT INTO recipe (mealsName, category,area, instruction ,imageUrl,youtube )
-VALUES('mansef','meat','jordan','mansef is delicuos','image link','youtube link');
 
