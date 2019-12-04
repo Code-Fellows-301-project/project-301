1a7f7520e0890cd5e6493e0a557c6748ec6cebff
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-12-04 22:45:42 +0200

    flow of our website

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ function deleteMeal(req, res){
   let values = [req.params.id];
   let SQL = `DELETE FROM recipe WHERE id=$1;`;
   client.query(SQL, values)
-    .then(res.redirect('/saved'));
+    .then(res.redirect('/saved'))
+    .catch(error => errorHandler(error, res));
 }
 function getUpdateForm(req, res){
   let {mealsname, category, area, instruction, imageurl, youtube,} = req.body;
@@ -45,14 +46,16 @@ function updateMeal(req, res){
   const SQL = 'UPDATE recipe SET mealsname=$1, category=$2, area=$3, instruction=$4,  imageurl=$5, youtube=$6 WHERE id=$7';
   let values = [mealsname, category, area, instruction, imageurl, youtube, req.params.id];
   client.query(SQL, values)
-    .then(res.redirect('/saved'));
+    .then(res.redirect('/saved'))
+    .catch(error => errorHandler(error, res));
 }
 function getFromDataBase(req, res){
   let sql = `SELECT * FROM recipe`;
   client.query(sql)
     .then(data => {
       res.render('pages/saved', { mealsArray: data.rows,});
-    });
+    })
+    .catch(error => errorHandler(error, res));
 }
 function addToDataBase(req, res){
   let {mealsname, category, area, instruction, imageurl, youtube,} = req.body;
@@ -61,14 +64,16 @@ function addToDataBase(req, res){
   client.query(SQL, values)
     .then(()=>{
       getFromDataBase(req, res);
-    });
+    })
+    .catch(error => errorHandler(error, res));
 }
 function mealForm(req,res) {
   res.render('pages/index');
 }
 function mealsHandler(req, res){
   getMeal(req.body.meal, res)
-    .then( (mealData) => res.status(200).json(mealData) );
+    .then( (mealData) => res.status(200).json(mealData) )
+    .catch(error => errorHandler(error, res));
 }
 function getMeal(meal, res) {
   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${meal}`;
@@ -80,7 +85,8 @@ function getMeal(meal, res) {
     })
     .then(results => {
       res.render('pages/meals', { mealsArray: results, });
-    });
+    })
+    .catch(error => errorHandler(error, res));
 }
 function Meal(meal, data) {
   this.search_query = meal;
@@ -90,7 +96,8 @@ function Meal(meal, data) {
 }
 function recipeHandler(req,res) {
   getRecipe(req.body.id,res)
-    .then( (recipeData) => res.status(200).json(recipeData) );
+    .then( (recipeData) => res.status(200).json(recipeData) )
+    .catch(error => errorHandler(error, res));
 }
 function getRecipe(recipe,res) {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe}`;
@@ -100,7 +107,8 @@ function getRecipe(recipe,res) {
     })
     .then(results => {
       res.render('pages/details', { meal: results, });
-    });
+    })
+    .catch(error => errorHandler(error, res));
 }
 function Recipe(recipe, data) {
   this.search_query = recipe;
@@ -110,6 +118,10 @@ function Recipe(recipe, data) {
   this.instruction = data.body.meals[0].strInstructions;
   this.imageurl = data.body.meals[0].strMealThumb;
   this.youtube = data.body.meals[0].strYoutube;
+}
+function errorHandler(err, res){
+  console.log(err);
+  res.status(500).render('pages/error');
 }
 /********************** Error *************************/
 app.use('*', (request, response) => {
