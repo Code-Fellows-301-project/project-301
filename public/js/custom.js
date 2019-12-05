57314a88d0f227f077b4f65179c06ac85e141c56
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-12-05 09:33:03 +0200

    adding meal video

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,19 +1,32 @@
 'use strict';
 
-let sample1 = "https://www.youtube.com/watch?v=QqdcCHQlOe0";
-let sample2 = "https://www.youtube.com/watch?v=bXKWu4GojNI";
+// let sample1 = "https://www.youtube.com/watch?v=QqdcCHQlOe0";
+// let sample2 = "https://www.youtube.com/watch?v=bXKWu4GojNI";
 
-  let stringVideo = sample2.substring(32, sample2.length - 1);
-console.log('stringVideo : ', stringVideo);
+//   let stringVideo = sample2.substring(32, sample2.length - 1);
+// console.log('stringVideo : ', stringVideo);
 
-let id = $('#mealVideo').attr("data-id");//.data('id');
+// let jj = $('#mealVideo').data('id');////.attr("data-id")
+// console.log("jj", jj);
+
+let id = $('#mealVideo').data('id');
 console.log("ID", id);
+let value = $('#youtube').val();
+console.log('value : ', value);
+$('#savedVideo').attr('src',value);
+
+
 $.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
 .then(data=>{
-  let str = data.body.meals[0].strYoutube;
-  let stringVideo = str.substring(30, str.length - 1);
+  console.log('data : ', data);
+  let str = data.meals[0].strYoutube;
+  console.log('str : ', str);
+  let stringVideo = str.substring(32, str.length);
 console.log('stringVideo : ', stringVideo);
-  $('#mealVideo').attr('src',`https://www.youtube.com/embed/${stringVideo}`);
+
+let videostring = `https://www.youtube.com/embed/${stringVideo}`;
+console.log('videostring : ', videostring);
+  $('#mealVideo').attr('src',videostring);
 });
 
 if(document.getElementById("textArea")){
